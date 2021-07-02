59744df9d70f33e6bf92878a931d1af61e90df2b
Ibraheem <ibraheem.shwiki@gmail.com>
2021-07-02 14:09:06 +0300

    1

---
+++ Zeno/src/main/java/com/Zenoproject/Zeno/ZenoApplication.java
@@ -0,0 +1,13 @@
+package com.Zenoproject.Zeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenoApplication.class, args);
+	}
+
+}
---
+++ Zeno/src/test/java/com/Zenoproject/Zeno/ZenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zenoproject.Zeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
